0feb089747840bf66bc24e8d7c5b471a4ef8b615
shweta_patel22@outlook.com <shweta_patel22@outlook.com>
2023-01-26 15:15:13 -0500

    Branch Testing

---
+++ ssdtGithub/SP_Ins_test.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[SP_Ins_test]
+	@FName varchar(50),
+	@LName varchar(50),
+	@TellNo varchar(50)
+	
+AS
+	insert into Test (Fname,LName,TellNo)
+	VALUES (@FName,@LName,@TellNo)
+RETURN 0
